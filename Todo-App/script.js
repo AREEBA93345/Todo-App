221e1655c6713532245381f37ea8101322f1ead9
Areeba <areebakhan93345@gmail.com>
2026-08-22 19:51:00 +0530

    JS learning

---
+++ Todo-App/script.js
@@ -0,0 +1,80 @@
+console.log("Welcome Areeba");
+//gives welcome areeba on the console
+/*alert("Welcome")
+gives welcome pop-up */
+
+ //prompt("Hello");
+//prompt("Hello");
+
+
+
+//Arithimatic Operator
+/*let a = 4;
+let b = 5;
+console.log("a = " , a , " & b = " , b);
+console.log("a + b = " , a + b);
+console.log("a - b = " , a - b);
+console.log("a * b = " , a * b);
+console.log("a / b = " , a / b);
+console.log("a % b = " , a % b);
+console.log("a ** b = " , a ** b);
+*/
+
+/*unary operators
+let a = 3;
+let b = 5;
+
+a++; // a+1=4
+console.log("a = " , a);
+b--; //b-1=4
+console.log("b = " , b );
+*/
+
+/*pre & post
+let a= 5;
+let b= 6;
+console.log("--a = " , --a);
+console.log("a = " , a);
+console.log("b-- = " , b--);
+console.log("b = " , b);
+*/
+
+/*Logical operator
+let a = 6;
+let b = 5;
+
+let cond1 = a < b ;
+let cond2 = a ===6 ;
+
+console.log("cond1 && cond2 = " , cond1 && cond2);
+console.log("cond1 || cond2 = " , cond1 || cond2);
+console.log("!(6<5)" , !6<5 );*/
+
+/*Conditional statement
+let age = 17;
+if (age > 18) {
+    console.log(" You Can Vote");
+}
+
+else  
+    console.log("Not elligiable");*/
+/*
+let number = 6;
+if (number%2===0) {
+    console.log("EVEN");   
+}
+else {console.log("ODD");
+}*/
+
+/*Ternary operator
+ let age = 21;
+ let result = age>= 18? "adult" : "not adult" ;
+ console.log(result);
+ */
+
+
+
+
+
+
+
