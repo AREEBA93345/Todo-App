045928b3c245c7314be4750161ea56653b2085f5
Areeba <areebakhan93345@gmail.com>
2026-08-24 20:39:31 +0530

    JS practice

--- Todo-App/script.js
+++ Todo-App/script.js
@@ -72,6 +72,25 @@ else {console.log("ODD");
  console.log(result);
  */
 
+/*Practie score = grade
+ let score = 77;
+ let Grade;
+ if (score>=90 && score<=100) {
+    console.log("Grade = A");
+ }
+ else if (score>=80 && score<=89) {
+    console.log("Grade = B");
+ }
+ else if (score>=70 && score<=79) {
+    console.log("Grade = C");
+ }
+else if (score>=60 && score <=69) {
+    console.log("Grade = D");
+}
+ else console.log("Fail");
+ */
+
+ 
 
 
 
